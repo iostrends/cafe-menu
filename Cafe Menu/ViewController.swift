57575b05d5ece15e9470140a30cad50986eb0bf5
iostrends <afteryou725@outlook.com>
2021-10-03 12:59:48 -0700

    Initial Commit

---
+++ Cafe Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe Menu
+//
+//  Created by Spruce Tree on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
